361e8ea791a4e0f4162298c1a87eb60b1de85949
PMS.UI <kza91@hotmail.com>
2023-09-05 14:00:53 +0200

    section12done

--- src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -7,44 +7,44 @@ import { CategoryService } from '../../category/services/category.service';
 import { Category } from '../../category/Models/category.model';
 import { UpdateBlogPost } from '../models/update-blog-post.model';
 
+
 @Component({
   selector: 'app-edit-blogpost',
   templateUrl: './edit-blogpost.component.html',
   styleUrls: ['./edit-blogpost.component.css']
 })
-export class EditBlogpostComponent implements OnInit, OnDestroy{
-  
+export class EditBlogpostComponent implements OnInit, OnDestroy {
   id: string | null = null;
   model?: BlogPost;
-  categories$?: Observable<Category[]>;
-  selectedCategories?: string[];
+  categories$? : Observable<Category[]>;
+  selectedCategories?: string[]
 
 
   routeSubscription?: Subscription;
-  updateBLogPostSubscription?: Subscription;
-  getBLogPostSubscription?: Subscription;
+  updateBlogPostSubscription?: Subscription;
+  getBlogPostSubscription?: Subscription;
+  deleteBlogPostSubscription?: Subscription;
 
 
-  constructor(private route: ActivatedRoute, 
-    private blogpostService : BlogPostService,
-    private categoryServide : CategoryService,
-    private router : Router)
-     {
+  constructor(private route: ActivatedRoute,
+    private blogPostService: BlogPostService,
+    private categoryService: CategoryService,
+    private router:Router) {
 
   }
-  
-  
+
+
   ngOnInit(): void {
-    this.categories$ = this.categoryServide.getAllCategories();
+    this.categories$ = this.categoryService.getAllCategories();
 
 
-   this.routeSubscription =  this.route.paramMap.subscribe({
+    this.routeSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
 
-        // GET BlogPost From API
-        if(this.id){
-          this.getBLogPostSubscription = this.blogpostService.getBlogPostById(this.id).subscribe({
+        // Get BlogPost From API
+        if (this.id) {
+          this.getBlogPostSubscription = this.blogPostService.getBlogPostById(this.id).subscribe({
             next: (response) => {
               this.model = response;
               this.selectedCategories = response.categories.map(x => x.id);
@@ -52,13 +52,15 @@ export class EditBlogpostComponent implements OnInit, OnDestroy{
           });
           ;
         }
+
       }
-    })
+    });
   }
 
-  onFormSubmit(): void{
+  onFormSubmit(): void {
+    // Convert this model to Request Object
     if (this.model && this.id) {
-      var updateBLogPost: UpdateBlogPost =  {
+      var updateBlogPost: UpdateBlogPost = {
         author: this.model.author,
         content: this.model.content,
         shortDescription: this.model.shortDescription,
@@ -70,18 +72,32 @@ export class EditBlogpostComponent implements OnInit, OnDestroy{
         categories: this.selectedCategories ?? []
       };
 
-      this.updateBLogPostSubscription = this.blogpostService.updateBlogPost(this.id, updateBLogPost)
+      this.updateBlogPostSubscription = this.blogPostService.updateBlogPost(this.id, updateBlogPost)
       .subscribe({
         next: (response) => {
-          this.router.navigateByUrl('/admin/blogposts')
+          this.router.navigateByUrl('/admin/blogposts');
         }
       });
     }
 
   }
+
+  onDelete(): void {
+    if (this.id) {
+      // call service and delete blogpost
+      this.deleteBlogPostSubscription = this.blogPostService.deleteBlogPost(this.id)
+      .subscribe({
+        next: (response) => {
+          this.router.navigateByUrl('/admin/blogposts');
+        }
+      });
+    }
+  }
+
   ngOnDestroy(): void {
     this.routeSubscription?.unsubscribe();
-    this.updateBLogPostSubscription?.unsubscribe();
-    this.getBLogPostSubscription?.unsubscribe();
+    this.updateBlogPostSubscription?.unsubscribe();
+    this.getBlogPostSubscription?.unsubscribe();
+    this.deleteBlogPostSubscription?.unsubscribe();
   }
 }
--- src/app/features/blog-post/services/blog-post.service.ts
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -20,10 +20,16 @@ export class BlogPostService {
   getAllBlogPosts() : Observable<BlogPost[]> {
     return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/blogposts`);
   }
-  getBlogPostById(id: string): Observable<BlogPost>{
-    return this.http.get<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`)
+
+  getBlogPostById(id: string): Observable<BlogPost> {
+    return this.http.get<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`);
   }
+
   updateBlogPost(id: string, updatedBlogPost: UpdateBlogPost): Observable<BlogPost> {
     return this.http.put<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`, updatedBlogPost);
+  }
+
+  deleteBlogPost(id: string): Observable<BlogPost> {
+    return this.http.delete<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`);
   }
 }

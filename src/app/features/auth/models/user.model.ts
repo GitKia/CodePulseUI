81b647e84b7e1c04b82411ee10e4b1692bed6416
PMS.UI <kza91@hotmail.com>
2023-09-10 14:10:08 +0200

    118. UI - Auth Service and Navbar Changes

---
+++ src/app/features/auth/models/user.model.ts
@@ -0,0 +1,4 @@
+export interface User {
+    emial: string;
+    roles: string[];
+}
--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { LoginRequest } from '../models/log-request.model';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { LoginResponse } from '../models/login-response.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  $user = new BehaviorSubject<User |undefined>(undefined)
 
   constructor(private http: HttpClient) { }
 
@@ -18,4 +21,17 @@ export class AuthService {
       password: requst.password
     })
   }
+
+
+  setUser(user: User): void {
+
+    this.$user.next(user);
+    localStorage.setItem('user-email', user.emial);
+    localStorage.setItem('user-roles', user.roles.join(','));
+  }
+
+  user() : Observable<User | undefined> {
+    return this.$user.asObservable();
+  }
+
 }
